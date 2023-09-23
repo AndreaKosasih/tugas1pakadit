fb7d280fd11df7a36cad071226084dea441d90d7
Andreas Teguh Santoso Kosasih <andreastegusks@gmial.com>
2023-09-23 22:48:52 +0700

    second commit

---
+++ tugas2cond.cpp
@@ -0,0 +1,85 @@
+/*
+Nama Program : Konversi format 12 menjadi 24 jam
+Nama         : Andreas Teguh Santoso Kosasih
+NPM          : 140810230047
+Tanggal Buat : Minggu, 18 September 2023
+Deskripsi    : Mengkonversi format 12 jam menjadi 24 jam 
+*/
+
+#include <iostream>
+#include <string>
+#include <iomanip> 
+using namespace std;
+
+int main () {
+    typedef struct {int jj; 
+                    int mm;
+                    int dd;
+                    string ap;
+                    char titikDua; 
+    } Jam; 
+
+    Jam j; 
+    
+    cout << "Masukkan Waktu Sistem 12 Jam" << endl;
+    cout << "Masukkan jam : menit : detik AM/PM: "; cin >> j.jj; cin >> j.titikDua; cin >> j.mm; cin >> j.titikDua; cin >> j.dd; cin >> j.ap; 
+
+    cout << "Jam dalam format 12 Jam adalah: " << setw(2) << setfill('0') << j.jj << ":" 
+                                               << setw(2) << setfill('0') << j.mm << ":" 
+                                               << setw(2) << setfill('0') << j.dd << " " 
+                                               << j.ap << endl;
+    if (j.jj != 12 && j.ap == "PM") {
+        j.jj += 12;
+    }
+
+    if (j.jj == 12 && j.ap == "AM") {
+        j.jj = 0;
+    }
+
+    cout << "Jam dalam format 24 Jam adalah = " << setw(2) << setfill('0') << j.jj << ":" 
+                                                << setw(2) << setfill('0') << j.mm << ":"
+                                                << setw(2) << setfill('0') << j.dd << endl;
+    return 0;
+}
+
+// input : 07:05:45 PM
+// output : 19:05:45 
+
+/*
+    Sistem 12-jam adalah jangka waktu 24 jam sehari yang dibagi menjadi dua waktu: a.m. dan p.m. 
+    Setiap waktu terdiri dari kelipatan 12 jam: 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, dan 11. 
+    Siklus 24 jam/hari dimulai pada 12 tengah malam, lalu 12 siang, dan berlanjut hingga tengah malam pada akhir hari. 
+*/
+
+/*
+Mengubah format 12 jam menjadi 24 jam 
+1. Gunakan 00.00 untuk menandakan tengah malam dalam format 24 jam! (untuk AM)
+2. Hapus AM untuk jam antara 01.00 dan 11.59 (untuk AM)
+3. Biarkan jam di siang hari tetap sedemikian adanya, hapus saja PM. 12.00 - 12.59 (untuk PM)
+4. Tambahkan 12 untuk jam antara 01.00 PM dan 11.59 PM dan hapus PM. Maka menjadi 13.00- 23.59! (untuk PM)
+*/
+
+/*
+    if (j.jj >= 0 && j.jj <= 9) {
+        cout << "Jam sekarang adalah = " << "0" << j.jj << ":" << j.mm << ":" << j.dd << " " << j.ap << endl;
+    } else if (j.mm >= 0  && j.mm <= 9) {
+        cout << "Jam sekarang adalah = " << j.jj << ":" << "0" << j.mm << ":" << j.dd << " " << j.ap << endl;
+    } else if (j.dd >= 0 && j.dd <= 9) {
+        cout << "Jam sekarang adalah = " << j.jj << ":" << j.mm << ":" << "0" << j.dd << " " << j.ap << endl;
+    } else if ((j.jj >= 0 && j.jj <= 9) && (j.mm >= 0  && j.mm <= 9)) {
+        cout << "Jam sekarang adalah = " << "0" << j.jj << ":" << "0" << j.mm << ":" << j.dd << " " << j.ap << endl;
+    } else if ((j.jj >= 0 && j.jj <= 9) && (j.dd >= 0  && j.dd <= 9)) {
+        cout << "Jam sekarang adalah = " << "0" << j.jj << ":" << j.mm << ":" << "0" << j.dd << " " << j.ap << endl;
+    } else if ((j.mm >= 0 && j.mm <= 9) && (j.dd >= 0  && j.dd <= 9)) {
+        cout << "Jam sekarang adalah = " << j.jj << ":" << "0" << j.mm << ":" << "0" << j.dd << " " << j.ap << endl;
+    } else if ((j.jj >= 0 && j.jj <= 9) && (j.mm >= 0  && j.mm <= 9) && (j.dd >= 0  && j.dd <= 9)) {
+        cout << "Jam sekarang adalah = " << "0" << j.jj << ":" << "0" << j.mm << ":" << "0" << j.dd << " " << j.ap << endl;
+    } else {
+        cout << "Jam sekarang adalah = " << j.jj << ":" << j.mm << ":" << j.dd << " " << j.ap << endl;
+    }
+*/
+
+
+
+
+
